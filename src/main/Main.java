7127a2daade2f22efab3804073e632894ded7e7e
Deus <rogerferraro256@gmail.com>
2019-05-24 21:28:20 -0300

    push

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,6 @@
 package main;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import windows.MainWindow;
 
